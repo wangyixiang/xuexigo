1a432db67517d3452f6e46fe1db394b89429802d
wangyixiang.linux <wangyixiang.linux@qq.com>
2013-03-30 23:49:20 +0800

    Adding my fun experience for trying channel.

---
+++ concurrency7/wyxfun1.go
@@ -0,0 +1,35 @@
+package concurrency7
+
+import (
+	"fmt"
+	"time"
+)
+
+var wyxch1 chan int
+var wyxch2 chan int
+
+func catcher(sleep int) {
+	var count uint64 = 0
+	for <-wyxch1 != 0 {
+		count += 1
+		time.Sleep(time.Duration(sleep) * time.Nanosecond)
+	}
+
+	fmt.Printf("I sleep %d nanoseconds, I cautch %d \n", sleep, count)
+	wyxch2 <- sleep
+}
+
+func Wyxfun1_1() {
+	catchers := []int{0, 100, 10000}
+	wyxch1 = make(chan int)
+	wyxch2 = make(chan int, 30)
+	for _, value := range catchers {
+		go catcher(value)
+	}
+	for i := uint64(0); i <= 10000000; i++ {
+		wyxch1 <- 1
+	}
+	close(wyxch1)
+	<-wyxch2
+	time.Sleep(1000 * time.Millisecond)
+}
