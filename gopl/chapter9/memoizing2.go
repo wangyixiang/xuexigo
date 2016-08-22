4a9f1bc405e226d17ae964a86456c3bc82b8d7fc
wangyixiang <wangyixiang.linux@qq.com>
2016-08-22 13:03:46 +0800

    a concurrency non-blocking cache implementation but having race conditions.

---
+++ gopl/chapter9/memoizing2.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+	"net/http"
+	"io/ioutil"
+	"time"
+	"log"
+	"fmt"
+	"sync"
+)
+
+type Memo struct {
+	f     Func
+	cache map[string]result
+}
+
+type Func func(key string) (interface{}, error)
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+func NewMemo(f Func) *Memo {
+	return &Memo{
+		f:    f,
+		cache:    make(map[string]result),
+	}
+}
+
+func (memo *Memo) Get(key string) (interface{}, error) {
+	result, ok := memo.cache[key]
+	if !ok {
+		result.value, result.err = memo.f(key)
+		memo.cache[key] = result
+	}
+	return result.value, result.err
+}
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
+
+func incomingURLs() []string {
+	return []string{
+		"www.baidu.com",
+		"www.qq.com",
+		"www.gopl.io",
+		"www.baidu.com",
+		"www.qq.com",
+		"www.gopl.io",
+	}
+}
+
+func main() {
+	TestConcurrency()
+}
+
+func TestConcurrency() {
+	wg := sync.WaitGroup{}
+	memo := NewMemo(httpGetBody)
+	gstart := time.Now()
+	for _, url := range incomingURLs() {
+		wg.Add(1)
+		// this cause race condition
+		go func(url string) {
+			start := time.Now()
+			value, err := memo.Get("http://" + url)
+			if err != nil {
+				log.Print(err)
+			}
+			fmt.Printf("%s, %s, %d bytes\n", "http://" + url, time.Since(start), len(value.([]byte)))
+			wg.Done()
+		}(url)
+	}
+	wg.Wait()
+	fmt.Println(time.Since(gstart))
+
+}
